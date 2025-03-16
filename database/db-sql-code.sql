6e58c1a1e133e78efe798f6d67f3bb39d1bc07ce
Cliff Cummings <cliffc@sunburst-design.com>
2025-03-16 16:36:28 -0600

    Added closing ";" to final query

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -249,4 +249,4 @@ SELECT inv_make, inv_model, classification_name
 
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
-inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles')
+inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
