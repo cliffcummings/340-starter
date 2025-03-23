78d47ade7670b64cdedf2a00d77cb31a129d22f9
Cliff Cummings <cliffc@sunburst-design.com>
2025-03-23 08:53:06 -0600

    Added advanced error handler

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes - uses inventoryRoute.js file
 app.use("/inv", inventoryRoute)
 //--------------------------------------------------
@@ -44,10 +44,12 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){message = err.message}
+  else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
-    nav,
+    message,
+    nav
   })
 })
 
